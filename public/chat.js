dcf98ab0c9f5563da5958331523ef4462459e54a
mssjaney <janey.webdev@gmail.com>
2023-12-21 23:09:07 +0100

    setup & add frontend script

---
+++ public/chat.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    let messages = [];
+    let socket = io.connect('http://localhost:3700');
+
+    let field = document.querySelector(".field");
+    let sendBtn = document.querySelector(".send");
+    let content = document.getElementById("content");
+
+    socket.on('message', function(data) {
+
+        if (data.message) {
+            messages.push(data.message);
+
+            let html = "";
+            for (let i=0; i < messages.length; i++) {
+                html += messages[i] + "<br />";
+            }
+
+            content.innerHTML = html;
+            
+        } else {
+            console.log("Run into a problem: ", data);
+        }
+    });
+
+    sendBtn.onclick = function() {
+        let text = field.value;
+        socket.emit('send', { message: text });
+        field.value = "";
+    }
+}
