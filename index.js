7fb71a38dbfa7ba4e7c8b40642ce7213b462eb3f
mssjaney <janey.webdev@gmail.com>
2023-12-21 22:23:50 +0100

    setup socket.io

--- index.js
+++ index.js
@@ -6,9 +6,18 @@ app.set('views', __dirname + '/tpl');
 app.set('view engine', "jade");
 app.engine('jade', require('jade').__express);
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
     res.render("page");
 });
 
-app.listen(port);
-console.log("Listening on port: ", port);
+app.use(express.static(__dirname + '/public'));
+
+let server = app.listen(port);
+let io = require('socket.io')(server);
+
+io.sockets.on('connection', function(socket) {
+    socket.emit('message', { message: 'Welcome to the chat board' });
+    socket.on('send', function(data) {
+        io.sockets.emit('message', data);
+    });
+});
